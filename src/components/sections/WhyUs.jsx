9bf340f23930716a246b8e3114150c35d685a818
Joshua Karr <s123325@ltisdschools.net>
2025-11-20 09:10:25 -0600

    added icons to the why us section

--- src/components/sections/WhyUs.jsx
+++ src/components/sections/WhyUs.jsx
@@ -6,6 +6,12 @@ import AppImage from '@assets/images/carousel/UXDesign.png'
 import WebsiteImage from '@assets/images/carousel/Software.png'
 import LandingPageImage from '@assets/images/carousel/appDesign.png'
 
+import { FaMoneyBillWave } from "react-icons/fa";
+import { FaHandshake } from "react-icons/fa";
+import { LuAtom } from "react-icons/lu";
+import { PiBlueprint } from "react-icons/pi";
+
+
 const WhyUs = () => {
 
   const [openIndex, setOpenIndex] = useState(0);
@@ -14,25 +20,25 @@ const WhyUs = () => {
     {
       title: 'Built for You',
       body: 'We build your product with you in mind. We listen to your needs and deliver a product that goes beyond your expectations.',
-      icon: '⚡',
+      icon: <LuAtom />,
       display: AppImage
     },
     {
       title: 'Affordable Pricing',
       body: 'No surprises, no hidden fees. We believe in honest pricing that respects your startup budget. We are open to negotiating custom pricing for your project.',
-      icon: '💰',
+      icon: <FaMoneyBillWave />,
       display: WebsiteImage
     },
     {
       title: 'Founder-to-Founder',
       body: 'We speak entrepreneur. We understand the unique challenges you face and deliver solutions that truly support your vision.',
-      icon: '🤝',
+      icon: <FaHandshake />,
       display: LandingPageImage
     },
     {
       title: 'Built Better',
       body: 'With deep expertise in enterprise-grade web technologies like Amazon Web Services and Microsoft Azure, we build scalable solutions designed to grow alongside your business.',
-      icon: '🤝',
+      icon: <PiBlueprint />,
       display: LandingPageImage
     },
   ]
@@ -46,9 +52,9 @@ const WhyUs = () => {
             {items.map((item, index) => (
               <div className={`item ${openIndex === index ? 'open' : ''}`} key={index} onClick={() => setOpenIndex(index)}>
                 <div className="acc-header">
-                  <span className="icon-emoji" style={{ fontSize: '24px' }}>
+                  <div className="icon-container">
                     {item.icon}
-                  </span>
+                  </div>
                   <h3>{item.title}</h3>
                 </div>
                 <div className="body">
